d5d84c508277c23b9a69e854db86a3e2ad4bd379
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2022-04-12 00:43:57 +0100

    User Table

--- src/components/colors.js
+++
@@ -1,4 +0,0 @@
-export const COLORS = {
-    main: "#4EA6DE",
-    main_brigther: "#95B8CF"
-}
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,45 +1,97 @@
 import React, { useState, useEffect } from 'react';
 import { apiURL } from '../components/apiURL';
 import { TokenContext } from '../components/context';
+import Table, { SelectColumnFilter } from '../components/Table';
 import './Dashboard.css';
+
+// const getOrganizationUsers = () => [
+//   {
+//     id: 1,
+//     name: "Kévin Domingues",
+//     address: "Rua casrwaseasfjns",
+//     organization: "Food to Share"
+//   },
+//   {
+//     id: 2,
+//     name: "Bruno Pereira",
+//     address: "Rua casrwaseasfjns",
+//     organization: "Food to Share"
+//   }
+// ]
 
 function Users() {
 
-  const { tokenContext } = React.useContext(TokenContext);
+    const tokenContext = React.useContext(TokenContext);
+
+    const [users, setUsers] = useState([]);
+
+    const columns = React.useMemo(
+      () => [
+        {
+          Header: "ID",
+          accessor: "id"
+        },
+        {
+          Header: "Name",
+          accessor: "name"
+        },
+        {
+          Header: "Email",
+          accessor: "email"
+        },
+        {
+          Header: "Address",
+          accessor: "address"
+        },
+        {
+          Header: "Organization",
+          accessor: "organization",
+          Filter: SelectColumnFilter,
+          filter: "includes",
+        },
+      ], []
+    )
 
   const getOrganizationUsers = async() => {
     try {
           const requestOptions = {
             method: 'GET',
-            hedares: {
+            headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer '+tokenContext
+                'Authorization': `Bearer ${tokenContext}`
             }
           }
-        
-          let response = await fetch(
-            `${apiURL}/users`, requestOptions
-            );
           
-          let json = await response.json();
-    
-          console.log(json);
-          setUsers(json);
-          return json;
+          let response = await fetch(
+            `${apiURL}/users/`, requestOptions
+            );
+
+          if(response.status === 200){
+            let json = await response.json();
+            
+            setUsers(json);
+            return;
+          }
+          
         } catch (error) {
           console.error(error);
         }
   }
 
-  const [users, setUsers] = useState(getOrganizationUsers());
+  useEffect(() => {
+    getOrganizationUsers();
+  }, []);
 
   return (
     <>
       <div className="p-7 flex-1 h-screen">
         <h1 className="text-2xl font-semibold">
-        Users
+          Users
         </h1>
+        <div className='mt-4'>
+          <Table columns={columns} data={users} />
+        </div>
       </div>
     </>
   );
